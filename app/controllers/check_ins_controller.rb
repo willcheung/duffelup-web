cd3f9628a1d8175c67e7400a8e3660cac3f28fc3
calvin681 <calvin681@cfd1b285-76da-49ef-8d89-d875fda4f682>
2011-02-22 07:16:38 +0000

    only return is_public check-ins

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -97,6 +97,7 @@ class CheckInsController < ApplicationController
   # GET /check_ins/near_by.xml
   def near_by
     @check_ins = CheckIn.paginate(:all, :origin => [params[:lat], params[:lng]], :within => 2, :page => params[:page],
+      :conditions => ["is_public = ?", true],
       :order => "FLOOR(distance / 0.5), created_at DESC")
     respond_to do |format|
       format.xml { render :text => @check_ins.to_xml { |xml| xml.tag! 'next-page', @check_ins.next_page } }
