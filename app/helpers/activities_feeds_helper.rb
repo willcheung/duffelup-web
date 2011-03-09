8c35d0cad4b8b02e1980fddacb3e8f27f13cf2eb
willcheung <willcheung@cfd1b285-76da-49ef-8d89-d875fda4f682>
2011-03-09 05:04:01 +0000

    Added news feed to all users.

--- app/helpers/activities_feeds_helper.rb
+++ app/helpers/activities_feeds_helper.rb
@@ -1,6 +1,8 @@
 module ActivitiesFeedsHelper
   # only display feed when current_user is invited to the trip
   def display_private_duffel_feed(feed)
+    return false if current_user.nil? # don't display if user is not logged in
+    
     if !@users_by_trip_id.nil? and !@users_by_trip_id[feed.trip_id.to_s].nil? 
       @users_by_trip_id[feed.trip_id.to_s].each do |u| 
         if u.id == current_user.id
