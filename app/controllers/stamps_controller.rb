03bda86694f20dcc80b1f8f726f406d0032f6888
calvin681 <calvin681@cfd1b285-76da-49ef-8d89-d875fda4f682>
2011-03-21 06:09:32 +0000

    set 'awarded_to_current_user' flag from stamps achieved by the current user

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -5,9 +5,14 @@ class StampsController < ApplicationController
   # GET /stamps.xml
   def index
     @stamps = Stamp.find(:all)
+    if current_user
+      achievement_map = Hash.new(false)
+      Achievement.find_all_by_user_id(current_user.id).each { |a| achievement_map[a.stamp_id] = true }
+      @stamps.each { |s| s.awarded_to_current_user = achievement_map[s.id] }
+    end
 
     respond_to do |format|
-      format.xml  { render :xml => @stamps }
+      format.xml { render :text => @stamps.to_xml(:methods => [:awarded_to_current_user]) }
     end
   end
 
--- spec/controllers/stamps_controller_spec.rb
+++ spec/controllers/stamps_controller_spec.rb
@@ -4,15 +4,26 @@ describe StampsController do
   fixtures :all
   
   before(:each) do
+    @user = users(:quentin)
     controller.stub!(:logged_in?).and_return(true)
+    controller.stub!(:current_user).and_return(@user)
   end
   
   describe "GET index" do
-    it "assigns all check_ins as @check_ins" do
-      stamp = stamps(:statue_of_liberty)
+    it "assigns all stamps as @stamps" do
       get :index
-      assigns[:stamps].should == [stamp]
+      assigns[:stamps].size.should == 2
+      assigns[:stamps].should include stamps(:jetsetter)
+      assigns[:stamps].should include stamps(:statue_of_liberty)
     end
+    
+    it "set awarded_to_current_user for stamps achieved by the current user" do
+      get :index
+      assigns[:stamps].size.should == 2
+      assigns[:stamps].any? { |s| s.id = stamps(:jetsetter).id && !s.awarded_to_current_user }.should be_true
+      assigns[:stamps].any? { |s| s.id = stamps(:statue_of_liberty).id && s.awarded_to_current_user }.should be_true
+    end
+    
   end
 
 end
