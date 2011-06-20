cf334bae82010bf5bf32f9cd69156c83c1ed08d8
willcheung <willcheung@cfd1b285-76da-49ef-8d89-d875fda4f682>
2011-06-20 06:51:49 +0000

    Added comments back to corkboard.

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -8669,6 +8669,467 @@ Observed = Behavior.create({
   }
 });
 
+/***********************
+	Slider.js
+************************/
+
+// script.aculo.us slider.js v1.8.2, Tue Nov 18 18:30:58 +0100 2008
+
+// Copyright (c) 2005-2008 Marty Haught, Thomas Fuchs
+//
+// script.aculo.us is freely distributable under the terms of an MIT-style license.
+// For details, see the script.aculo.us web site: http://script.aculo.us/
+
+if (!Control) var Control = { };
+
+// options:
+//  axis: 'vertical', or 'horizontal' (default)
+//
+// callbacks:
+//  onChange(value)
+//  onSlide(value)
+Control.Slider = Class.create({
+  initialize: function(handle, track, options) {
+    var slider = this;
+
+    if (Object.isArray(handle)) {
+      this.handles = handle.collect( function(e) { return $(e) });
+    } else {
+      this.handles = [$(handle)];
+    }
+
+    this.track   = $(track);
+    this.options = options || { };
+
+    this.axis      = this.options.axis || 'horizontal';
+    this.increment = this.options.increment || 1;
+    this.step      = parseInt(this.options.step || '1');
+    this.range     = this.options.range || $R(0,1);
+
+    this.value     = 0; // assure backwards compat
+    this.values    = this.handles.map( function() { return 0 });
+    this.spans     = this.options.spans ? this.options.spans.map(function(s){ return $(s) }) : false;
+    this.options.startSpan = $(this.options.startSpan || null);
+    this.options.endSpan   = $(this.options.endSpan || null);
+
+    this.restricted = this.options.restricted || false;
+
+    this.maximum   = this.options.maximum || this.range.end;
+    this.minimum   = this.options.minimum || this.range.start;
+
+    // Will be used to align the handle onto the track, if necessary
+    this.alignX = parseInt(this.options.alignX || '0');
+    this.alignY = parseInt(this.options.alignY || '0');
+
+    this.trackLength = this.maximumOffset() - this.minimumOffset();
+
+    this.handleLength = this.isVertical() ?
+      (this.handles[0].offsetHeight != 0 ?
+        this.handles[0].offsetHeight : this.handles[0].style.height.replace(/px$/,"")) :
+      (this.handles[0].offsetWidth != 0 ? this.handles[0].offsetWidth :
+        this.handles[0].style.width.replace(/px$/,""));
+
+    this.active   = false;
+    this.dragging = false;
+    this.disabled = false;
+
+    if (this.options.disabled) this.setDisabled();
+
+    // Allowed values array
+    this.allowedValues = this.options.values ? this.options.values.sortBy(Prototype.K) : false;
+    if (this.allowedValues) {
+      this.minimum = this.allowedValues.min();
+      this.maximum = this.allowedValues.max();
+    }
+
+    this.eventMouseDown = this.startDrag.bindAsEventListener(this);
+    this.eventMouseUp   = this.endDrag.bindAsEventListener(this);
+    this.eventMouseMove = this.update.bindAsEventListener(this);
+
+    // Initialize handles in reverse (make sure first handle is active)
+    this.handles.each( function(h,i) {
+      i = slider.handles.length-1-i;
+      slider.setValue(parseFloat(
+        (Object.isArray(slider.options.sliderValue) ?
+          slider.options.sliderValue[i] : slider.options.sliderValue) ||
+         slider.range.start), i);
+      h.makePositioned().observe("mousedown", slider.eventMouseDown);
+    });
+
+    this.track.observe("mousedown", this.eventMouseDown);
+    document.observe("mouseup", this.eventMouseUp);
+    document.observe("mousemove", this.eventMouseMove);
+
+    this.initialized = true;
+  },
+  dispose: function() {
+    var slider = this;
+    Event.stopObserving(this.track, "mousedown", this.eventMouseDown);
+    Event.stopObserving(document, "mouseup", this.eventMouseUp);
+    Event.stopObserving(document, "mousemove", this.eventMouseMove);
+    this.handles.each( function(h) {
+      Event.stopObserving(h, "mousedown", slider.eventMouseDown);
+    });
+  },
+  setDisabled: function(){
+    this.disabled = true;
+  },
+  setEnabled: function(){
+    this.disabled = false;
+  },
+  getNearestValue: function(value){
+    if (this.allowedValues){
+      if (value >= this.allowedValues.max()) return(this.allowedValues.max());
+      if (value <= this.allowedValues.min()) return(this.allowedValues.min());
+
+      var offset = Math.abs(this.allowedValues[0] - value);
+      var newValue = this.allowedValues[0];
+      this.allowedValues.each( function(v) {
+        var currentOffset = Math.abs(v - value);
+        if (currentOffset <= offset){
+          newValue = v;
+          offset = currentOffset;
+        }
+      });
+      return newValue;
+    }
+    if (value > this.range.end) return this.range.end;
+    if (value < this.range.start) return this.range.start;
+    return value;
+  },
+  setValue: function(sliderValue, handleIdx){
+    if (!this.active) {
+      this.activeHandleIdx = handleIdx || 0;
+      this.activeHandle    = this.handles[this.activeHandleIdx];
+      this.updateStyles();
+    }
+    handleIdx = handleIdx || this.activeHandleIdx || 0;
+    if (this.initialized && this.restricted) {
+      if ((handleIdx>0) && (sliderValue<this.values[handleIdx-1]))
+        sliderValue = this.values[handleIdx-1];
+      if ((handleIdx < (this.handles.length-1)) && (sliderValue>this.values[handleIdx+1]))
+        sliderValue = this.values[handleIdx+1];
+    }
+    sliderValue = this.getNearestValue(sliderValue);
+    this.values[handleIdx] = sliderValue;
+    this.value = this.values[0]; // assure backwards compat
+
+    this.handles[handleIdx].style[this.isVertical() ? 'top' : 'left'] =
+      this.translateToPx(sliderValue);
+
+    this.drawSpans();
+    if (!this.dragging || !this.event) this.updateFinished();
+  },
+  setValueBy: function(delta, handleIdx) {
+    this.setValue(this.values[handleIdx || this.activeHandleIdx || 0] + delta,
+      handleIdx || this.activeHandleIdx || 0);
+  },
+  translateToPx: function(value) {
+    return Math.round(
+      ((this.trackLength-this.handleLength)/(this.range.end-this.range.start)) *
+      (value - this.range.start)) + "px";
+  },
+  translateToValue: function(offset) {
+    return ((offset/(this.trackLength-this.handleLength) *
+      (this.range.end-this.range.start)) + this.range.start);
+  },
+  getRange: function(range) {
+    var v = this.values.sortBy(Prototype.K);
+    range = range || 0;
+    return $R(v[range],v[range+1]);
+  },
+  minimumOffset: function(){
+    return(this.isVertical() ? this.alignY : this.alignX);
+  },
+  maximumOffset: function(){
+    return(this.isVertical() ?
+      (this.track.offsetHeight != 0 ? this.track.offsetHeight :
+        this.track.style.height.replace(/px$/,"")) - this.alignY :
+      (this.track.offsetWidth != 0 ? this.track.offsetWidth :
+        this.track.style.width.replace(/px$/,"")) - this.alignX);
+  },
+  isVertical:  function(){
+    return (this.axis == 'vertical');
+  },
+  drawSpans: function() {
+    var slider = this;
+    if (this.spans)
+      $R(0, this.spans.length-1).each(function(r) { slider.setSpan(slider.spans[r], slider.getRange(r)) });
+    if (this.options.startSpan)
+      this.setSpan(this.options.startSpan,
+        $R(0, this.values.length>1 ? this.getRange(0).min() : this.value ));
+    if (this.options.endSpan)
+      this.setSpan(this.options.endSpan,
+        $R(this.values.length>1 ? this.getRange(this.spans.length-1).max() : this.value, this.maximum));
+  },
+  setSpan: function(span, range) {
+    if (this.isVertical()) {
+      span.style.top = this.translateToPx(range.start);
+      span.style.height = this.translateToPx(range.end - range.start + this.range.start);
+    } else {
+      span.style.left = this.translateToPx(range.start);
+      span.style.width = this.translateToPx(range.end - range.start + this.range.start);
+    }
+  },
+  updateStyles: function() {
+    this.handles.each( function(h){ Element.removeClassName(h, 'selected') });
+    Element.addClassName(this.activeHandle, 'selected');
+  },
+  startDrag: function(event) {
+    if (Event.isLeftClick(event)) {
+      if (!this.disabled){
+        this.active = true;
+
+        var handle = Event.element(event);
+        var pointer  = [Event.pointerX(event), Event.pointerY(event)];
+        var track = handle;
+        if (track==this.track) {
+          var offsets  = Position.cumulativeOffset(this.track);
+          this.event = event;
+          this.setValue(this.translateToValue(
+           (this.isVertical() ? pointer[1]-offsets[1] : pointer[0]-offsets[0])-(this.handleLength/2)
+          ));
+          var offsets  = Position.cumulativeOffset(this.activeHandle);
+          this.offsetX = (pointer[0] - offsets[0]);
+          this.offsetY = (pointer[1] - offsets[1]);
+        } else {
+          // find the handle (prevents issues with Safari)
+          while((this.handles.indexOf(handle) == -1) && handle.parentNode)
+            handle = handle.parentNode;
+
+          if (this.handles.indexOf(handle)!=-1) {
+            this.activeHandle    = handle;
+            this.activeHandleIdx = this.handles.indexOf(this.activeHandle);
+            this.updateStyles();
+
+            var offsets  = Position.cumulativeOffset(this.activeHandle);
+            this.offsetX = (pointer[0] - offsets[0]);
+            this.offsetY = (pointer[1] - offsets[1]);
+          }
+        }
+      }
+      Event.stop(event);
+    }
+  },
+  update: function(event) {
+   if (this.active) {
+      if (!this.dragging) this.dragging = true;
+      this.draw(event);
+      if (Prototype.Browser.WebKit) window.scrollBy(0,0);
+      Event.stop(event);
+   }
+  },
+  draw: function(event) {
+    var pointer = [Event.pointerX(event), Event.pointerY(event)];
+    var offsets = Position.cumulativeOffset(this.track);
+    pointer[0] -= this.offsetX + offsets[0];
+    pointer[1] -= this.offsetY + offsets[1];
+    this.event = event;
+    this.setValue(this.translateToValue( this.isVertical() ? pointer[1] : pointer[0] ));
+    if (this.initialized && this.options.onSlide)
+      this.options.onSlide(this.values.length>1 ? this.values : this.value, this);
+  },
+  endDrag: function(event) {
+    if (this.active && this.dragging) {
+      this.finishDrag(event, true);
+      Event.stop(event);
+    }
+    this.active = false;
+    this.dragging = false;
+  },
+  finishDrag: function(event, success) {
+    this.active = false;
+    this.dragging = false;
+    this.updateFinished();
+  },
+  updateFinished: function() {
+    if (this.initialized && this.options.onChange)
+      this.options.onChange(this.values.length>1 ? this.values : this.value, this);
+    this.event = null;
+  }
+});
+// -----------------------------------------------------------------------------------
+//
+//	Slida v1.0
+//	by Studio Skylab - http://www.studioskylab.com
+//
+//	For more information about this script visit:
+//	http://www.studioskylab.com/journal/item/slida_iphone_scriptaculous_javascript_slider_widget
+//
+//	Licensed under the Creative Commons Attribution 3.0
+//  License - http://creativecommons.org/licenses/by/3.0/
+//
+//  Please retain the header in this script.
+//
+// -----------------------------------------------------------------------------------
+
+Effect.Slida = Class.create();
+
+Object.extend(Object.extend(Effect.Slida.prototype, Effect.Base.prototype), 
+{
+  initialize: function(form)
+	{
+	  this.form             = $(form);
+	  this.isBuilt          = false;
+	  this.checkboxElement  = null;
+	  this.hasMoved         = false;
+	  
+	  var options = arguments[1] || {};
+
+	  if (this.form.tagName == 'FORM') 
+	  {
+	    var onLabel   = options.onLabel ? options.onLabel : 'On';
+  	  var offLabel  = options.offLabel ? options.offLabel : 'Off';
+	  
+  	  this.labels = [onLabel, offLabel];
+	  
+	    var formCheckboxes  = this.form.select('input[type=checkbox]');
+  	  if (formCheckboxes[0])
+  	  {
+  	    this.checkboxElement = formCheckboxes[0];
+  	    this.currentValue = this.checkboxElement.checked ? true : false;
+  	  }
+  	  else this.currentValue = false;
+  	  
+  	  this.buildSlida();
+    }
+  },
+  
+  buildSlida: function()
+	{
+	  if (!this.isBuilt)
+	  {
+	    var slidaHTML  = '<div class="slida"><div class="slidaTrack"><div class="slidaSlider"><span class="slidaLabel slidaLabelOff">' +    
+        this.labels[1] + '</span><div class="slidaHandle"></div><span class="slidaLabel slidaLabelOn">' + 
+        this.labels[0] + '</span></div></div><div class="slidaOverlayLeft"></div><div class="slidaOverlayRight"></div></div>'
+      
+      this.form.insert(
+	    { 
+	      before: slidaHTML
+	    });
+	    
+	    this.url              = this.form.action;	    
+	    
+	    this.slidaContainer  = Element.previous(this.form);	
+	    this.slider           = this.slidaContainer.select('.slidaSlider')[0];
+  	  this.track            = this.slidaContainer.select('.slidaTrack')[0];
+  	  this.handle           = this.slidaContainer.select('.slidaHandle')[0];
+  	  this.labels           = this.slidaContainer.select('.slidaLabel');
+  	  
+  	  var handleWidth       = this.handle.getWidth();
+  	  var trackWidth        = this.slidaContainer.getWidth();
+  	  var labelWidth        = 0;
+  	  
+  	  this.track.setStyle({ width: trackWidth + 'px' });
+  	  
+  	  this.labels.each(function(label) 
+  	  {
+  	    labelWidth = (trackWidth - handleWidth/2);
+  	    px = labelWidth + 'px';
+  	    label.setStyle({ width: px })  	    
+  	  }.bind(this))
+  	  
+  	  this.sliderRightPos = (trackWidth - handleWidth + 1) * -1;
+  	  this.handle.setStyle({ left: (labelWidth - handleWidth/2) + 'px' });
+  	  
+  	  this.slider.setStyle({ width: (trackWidth * 2 - handleWidth) + 'px' });
+  	  
+	    this.attachFunctionality();
+	    
+	    this.form.remove();
+	    
+	    this.isBuilt = true;
+	  }
+	},
+	
+	attachFunctionality: function()
+	{
+	  this.control = new Control.Slider(this.slider, this.track, 
+  	{
+  	  sliderValue:  this.currentValue ? 1 : 0,
+      onChange:     function(v)
+      {
+        if (!this.currentValue && v < 1) 
+        {
+          this.moveSlider(0);
+        }
+        else if (this.currentValue && v > 0)
+        {
+          this.moveSlider(1);
+        }
+        else 
+        {
+          this.currentValue = (v == 1) ? true : false;
+          this.ajaxUpdate();
+        }
+      }.bind(this),
+
+      onSlide: function(v)
+      {
+      }.bind(this)
+    }); 
+    
+    Event.observe(this.handle, 'mousedown', function(e)
+    {
+      this.mousedown = true;
+    }.bind(this));
+    
+    Event.observe(this.handle, 'mousemove', function(e)
+    {
+      if (this.mousedown)
+      {
+        this.hasMoved = true;
+      }
+    }.bind(this));
+    
+    Event.observe(this.handle, 'mouseup', function(e)
+    {
+      if (!this.hasMoved) this.toggleValue();
+      this.hasMoved   = false;
+      this.mousedown  = false;
+    }.bind(this));
+
+	},
+	
+	ajaxUpdate: function()
+	{
+	  //var url = this.url + '?' + this.checkboxElement.name + '=' + (this.currentValue == 1 ? 'on' : 'off');
+
+		//     new Ajax.Request(url, {
+		//   method: 'post',
+		//   onSuccess: function(transport) 
+		// 	{
+		//   }.bind(this)
+		// });
+		location.href = this.url;
+	},
+	
+	setValue: function(value)
+	{	  
+	  this.moveSlider(value); 
+	  this.currentValue = value;
+	  this.ajaxUpdate();
+	},
+	
+	toggleValue: function()
+	{
+	  this.setValue(!this.currentValue);
+	},
+	
+	moveSlider: function(value)
+	{
+	  if (value) 
+	  {
+	    new Effect.Move(this.slider, { x: this.sliderRightPos, mode: 'absolute', duration: 0.2 });
+	  }
+	  else 
+	  {
+	    new Effect.Move(this.slider, { x: 0, mode: 'absolute', duration: 0.2 });
+	  }
+	}
+});
+
+
 /******************************
 	Sliding tab
 	http://www.andrewsellick.com/44/sexy-sliding-menu-for-scriptaculous
