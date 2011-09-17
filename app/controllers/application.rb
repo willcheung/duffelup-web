1261da45e34117253a348331c985c6b917e7fd60
willcheung <willcheung@cfd1b285-76da-49ef-8d89-d875fda4f682>
2011-09-17 22:19:21 +0000

    Fixed flickraw require.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -180,6 +180,13 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_path
   end
   
+  # Use this to access the flickr API easily. You can type directly the flickr requests as they are described on the flickr website.
+  # require 'flickraw'
+  #
+  # recent_photos = flickr.photos.getRecent
+  # puts recent_photos[0].title
+  def flickr; $flickraw ||= FlickRaw::Flickr.new end
+  
   ########## Other Stuff ###########
   def admin?
     current_user.username == "will" or current_user.username == "duffelup"
--- lib/flickraw.rb
+++ lib/flickraw.rb
@@ -379,10 +379,3 @@ end
     end
   end
 end
-
-# Use this to access the flickr API easily. You can type directly the flickr requests as they are described on the flickr website.
-# require 'flickraw'
-#
-# recent_photos = flickr.photos.getRecent
-# puts recent_photos[0].title
-def flickr; $flickraw ||= FlickRaw::Flickr.new end
