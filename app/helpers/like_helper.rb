9d81c3496f2b39ea3bee078a5e09d761b6588db1
Will Cheung <willcheung@users.noreply.github.com>
2011-11-19 05:43:25 +0000

    added like in event#show

--- app/helpers/like_helper.rb
+++ app/helpers/like_helper.rb
@@ -1,6 +1,11 @@
 module LikeHelper
-  def liked?(event) 
+  def liked?(event, from_event_show=nil) 
     return false if not logged_in?
-    @likes.map(&:event_id).include?(event.id)
+    
+    if from_event_show
+      Like.find_by_user_id_and_event_id(current_user.id, event.id)
+    else
+      @likes.map(&:event_id).include?(event.id)
+    end
   end
 end
