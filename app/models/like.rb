6903d1558552b1310b7f130a6148431db6b4dc7a
Will Cheung <willcheung@users.noreply.github.com>
2011-11-20 19:21:35 +0000

    Added like counts.

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,4 +6,20 @@ class Like < ActiveRecord::Base
    validates_presence_of :user
    validates_presence_of :event
 
+   def self.count_likes(events)
+     likes = Hash.new
+     
+     tmp = Like.find_by_sql("SELECT event_id, count(`likes`.id) AS count_all 
+                        FROM `likes` 
+                        WHERE (`likes`.event_id in 
+                            (#{events.map(&:id).join(',')})) 
+                        group by likes.event_id;")
+                        
+      tmp.each do |l|
+        likes[l.event_id] = l.count_all
+      end
+      
+      return likes
+   end
+
 end
